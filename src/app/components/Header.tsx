507405ae0b00a09ea29720dbe452be76d96157a7
sami <msami.bese22seecs@student.nust.edu.pk>
2025-02-11 11:35:50 +0500

    header updated

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import Link from "next/link"
-import { MoonIcon } from "lucide-react"
-import { motion } from "framer-motion"
+import Link from "next/link";
+import Image from "next/image";
+import { MoonIcon } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
@@ -14,9 +15,12 @@ export default function Header() {
     >
       <nav className="container mx-auto px-6">
         <div className="flex items-center justify-between">
-          <motion.div whileHover={{ scale: 1.05 }} className="text-green-800 font-bold text-3xl">
-            Talk PSX
-          </motion.div>
+          <div className="flex items-center space-x-3">
+            <Image src="/logo.png" alt="Talk PSX Logo" width={200} height={200} />
+            <motion.div whileHover={{ scale: 1.05 }} className="text-green-800 font-bold text-3xl">
+              
+            </motion.div>
+          </div>
           <div className="hidden md:flex items-center space-x-8 font-bold">
             <Link href="#" className="text-green-700 hover:text-white text-2xl">
               Home
@@ -41,6 +45,5 @@ export default function Header() {
         </div>
       </nav>
     </motion.header>
-  )
+  );
 }
-
